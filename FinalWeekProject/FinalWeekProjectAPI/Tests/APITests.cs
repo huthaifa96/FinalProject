719f3ce52e12aa51a24350c79f5aeb068cf11248
Huthaifa Ahmed <huthaifa@hotmail.co.uk>
2020-10-16 08:51:26 +0100

    Friday morning

--- FinalWeekProject/FinalWeekProjectAPI/Tests/APITests.cs
+++ FinalWeekProject/FinalWeekProjectAPI/Tests/APITests.cs
@@ -18,15 +18,7 @@ namespace FinalWeekProjectAPI
             Assert.That(_Service.BooksLatestDTO.Book.Books[0].pages, Is.EqualTo(234));
         }
 
-        //[Test]
-        //public void AddingABook()
-        //{
-        //    string[] names = { "isbn", "title", "subTitle", "author", "publish_date", "publisher", "pages", "description", "website"};
-        //    string[] value = { "123", "LOTR", "test", "test", "2020-06-04T08:48:39.000Z", "test", "1", "test", "test"};
-        //    _Service.CallManager.AddBook(names, value);
-        //    _Service.GetResults();
-        //    Assert.That(_Service.BooksLatestDTO.Book.Books.ToString(), Is.EqualTo("LOTR"));
-        //}
+        
 
 
     }
